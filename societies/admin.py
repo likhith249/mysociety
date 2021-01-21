e0347fcc36a929f4a902dc52ab9b4bf2bd99dc39
Likhith <likhith.barmavath@mygate.in>
2021-01-21 18:52:51 +0530

    likhith

--- societies/admin.py
+++ societies/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from . models import Society,Member
+from . models import Society,Member,Dues
 
 admin.site.register(Society)
 admin.site.register(Member)
+admin.site.register(Dues)
